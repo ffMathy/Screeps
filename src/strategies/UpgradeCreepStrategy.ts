23ecdd0fd16128f95dec70b98f994095c2096cb1
Mathias Lorenzen <mathias.lorenzen@broelstaerk.dk>
2019-04-09 09:40:24 +0200

    qwd

--- src/strategies/UpgradeCreepStrategy.ts
+++ src/strategies/UpgradeCreepStrategy.ts
@@ -1,4 +1,5 @@
 import CreepDecorator, { CreepStrategy } from "CreepDecorator";
+import StrategyPickingCreepStrategy from "./StrategyPickingCreepStrategy";
 
 export default class UpgradeCreepStrategy implements CreepStrategy {
   get name() {
@@ -6,6 +7,9 @@ export default class UpgradeCreepStrategy implements CreepStrategy {
   }
 
   tick(creep: CreepDecorator) {
+    if(creep.creep.carry.energy == 0)
+      return creep.setStrategy(new StrategyPickingCreepStrategy());
+      
     if (creep.creep.upgradeController(creep.creep.room.controller) == ERR_NOT_IN_RANGE) {
       creep.creep.moveTo(creep.creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
     }
